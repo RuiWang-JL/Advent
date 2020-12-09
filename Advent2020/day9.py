58582b6c0c3be72d3ca2b52a0e540e1993f6f36e
RuiWang-JL <rui.wang.jl@gmail.com>
2020-12-09 23:05:18 +0100

    day9 optimized

--- Advent2020/day9.py
+++ Advent2020/day9.py
@@ -1,25 +1,28 @@
 import re
 import numpy as np
 from collections import deque
-
-
-
-def find(x, nums):
-    nums_need = []
-    for ele in nums:
-        if ele < x/2:
-            nums_need.append(x-ele)
-    if not (set(nums_need) & set(nums)):
-        print(x)
+from itertools import combinations
 
 with open('day9.txt') as f:
-    lines = [x.strip() for x in f.readlines()]
+    lines = [int(x.strip()) for x in f.readlines()]
+
 d = deque()
 l = 25
-for i in range(len(lines)):
-    d.append(int(lines[i]))
+#1
+for i,line in enumerate(lines):
+    d.append(line)
     if i < l:
         pass
     else:
-        find(int(lines[i]),d)
+        if not any(sum(a) == line for a in combinations(d,2)):
+            part1 = line
+            print(part1)
+            break
         d.popleft()
+#2
+for n in range(2,len(lines)):
+    for i in range(len(lines)-n):
+        s = lines[i:i+n]
+        if sum(s) == part1:
+            print(min(s) + max(s))
+            quit()
