748731a161663657865d96f4e08295836d175c84
RuiWang-JL <rui.wang.jl@gmail.com>
2022-12-04 22:56:55 +0100

    2018 day3

--- 2018/03.py
+++ 2018/03.py
@@ -1,5 +1,39 @@
-print('hello world')
-for i in range(3):
-    print(i)
-
-
+tot=0
+# with open('test.txt', 'r+') as f:
+with open('03.txt', 'r+') as f:
+    lines = [line.strip() for line in f.readlines()]
+    la = set()
+    ldup = set()
+    for line in lines:
+        idnum, a = line.split("@")
+        b, c = a.split(": ")
+        aa, bb = b.split(",")
+        cc, dd = c.split("x")
+        aa = int(aa)
+        bb = int(bb)
+        cc, dd = int(cc) , int(dd)
+        tot = cc*dd
+        for x in range (cc):
+            for y in range(dd):
+                if (aa+x, bb+y) not in ldup:
+                    if (aa+x, bb+y) not in la:
+                        la.add((aa+x, bb+y))
+                        tot-=1
+                    else:
+                        ldup.add((aa+x, bb+y))
+        
+    for line in lines:
+        idnum, a = line.split("@")
+        b, c = a.split(": ")
+        aa, bb = b.split(",")
+        cc, dd = c.split("x")
+        aa = int(aa)
+        bb = int(bb)
+        cc, dd = int(cc) , int(dd)
+        tot = cc*dd
+        for x in range (cc):
+            for y in range(dd):
+                if(aa+x,bb+y) not in ldup:
+                    tot-=1
+        if tot ==0:
+            print(idnum)
